906701e7fdd5c27827132a98cf08f2a12d9c37f1
zekailiu <zekailiu1993@gamil.com>
2016-11-01 11:52:03 -0700

    initial version of bubble_sort

---
+++ bubble_sort.py
@@ -0,0 +1,21 @@
+import pdb
+def bubble_sort(list):
+    list_len = len(list)
+    if list_len <= 1:
+        return
+    index = 0
+    while(index < list_len-1):
+        if list[index] > list[index+1]:
+            temp = list[index]
+            list[index] = list[index+1]
+            list[index+1] = temp
+        elif index == 0 and list[index] < list[index+1]:
+            break
+        index += 1
+        if index == list_len-1:
+            index = 0
+
+if __name__ == '__main__':
+    list = [8,7,6,5,4,3,2,1]
+    bubble_sort(list)
+    print list
